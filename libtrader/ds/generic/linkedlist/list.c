5b779e1340302b34c4795d1f2e176e21973246a2
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-06-07 15:19:37 +0300

    ds: add linkedlist implementation

---
+++ libtrader/ds/generic/linkedlist/list.c
@@ -0,0 +1,129 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+struct LinkedListNode *createLinkedListNode(void *data,
+					    struct LinkedListNode *next,
+					    struct LinkedListNode *prev)
+{
+	struct LinkedListNode *node =
+		(struct LinkedListNode *)malloc(sizeof(struct LinkedListNode));
+	node->data = data;
+	node->next = next;
+	node->prev = prev;
+	return node;
+}
+
+bool destroyLinkedListNode(struct LinkedListNode *node)
+{
+	assert(node);
+	free(node->data);
+
+	/* connect nodes */
+	if (node->prev && node->next) {
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+	} else if (node->prev)
+		node->prev->next = NULL;
+	else if (node->next)
+		node->next->prev = NULL;
+	else
+		return false;
+
+	free(node);
+	return true;
+}
+
+LinkedList *createLinkedList(size_t starting_size,
+			     struct LinkedListNode *starting_node)
+{
+	LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+	list->len = starting_size;
+	list->start = starting_node;
+
+	/* verify given nodes */
+	if (!linkedList_verify(list))
+		exit(1); /* TODO: add central error handling */
+
+	return list;
+}
+
+bool destroyLinkedList(LinkedList *list)
+{
+	assert(list);
+	linkedList_verify(list);
+
+	struct LinkedListNode *ticker = list->start;
+	while (list->len && list->start) {
+		ticker = list->start->next;
+		if (!destroyLinkedListNode(list->start))
+			return false;
+		list->start = ticker;
+		list->len--;
+	}
+	return true;
+}
+
+bool linkedlist_verify(LinkedList *list)
+{
+	assert(list);
+	/* verify given nodes */
+	struct LinkedListNode *ticker = list->start;
+	assert(ticker);
+	ticker = ticker->next;
+	for (size_t i = 1; i < list->len; i++) {
+		if (ticker && ticker->next)
+			ticker = ticker->next;
+		else
+			return false;
+	}
+	return true;
+}
+
+void linkedList_add(LinkedList *list, struct LinkedListNode *new_node,
+		    size_t position)
+{
+	assert(list);
+	assert(linkedList_verify(list));
+	assert(new_node);
+	assert(position || position == 0);
+	assert(list->len >= position);
+
+	struct LinkedListNode *node = linkedList_getNode(list, position);
+	assert(node);
+	if (node->next) {
+		node->next->prev = new_node;
+		node->next = new_node;
+	} else
+		node->next = new_node;
+	list->len++;
+}
+
+size_t linkedList_getPos(LinkedList *list, struct LinkedListNode *node)
+{
+	assert(list);
+	assert(node);
+
+	struct LinkedListNode *ticker = list->start;
+	for (size_t i = 0; i < list->len; i++)
+		if (ticker == node)
+			return i;
+		else
+			ticker = ticker->next;
+
+	return -1;
+}
+
+struct LinkedListNode *linkedList_getNode(LinkedList *list, size_t position)
+{
+	assert(list);
+	assert(position || position == 0);
+
+	struct LinkedListNode *ticker = list->start;
+	while (position) {
+		ticker = ticker->next;
+		position--;
+	}
+	return ticker;
+}
--- libtrader/ds/generic/linkedlist/list.h
+++ libtrader/ds/generic/linkedlist/list.h
@@ -1,26 +1,29 @@
 #ifndef DS_GENERIC_LINKED_LIST_H
 #define DS_GENERIC_LINKED_LIST_H
 
-typedef struct LinkedListNode {
+#include <stdbool.h>
+
+struct LinkedListNode {
 	void *data;
-	LinkedListNode *next;
-	LinkedListNode *prev;
-} LinkedListNode;
-LinkedListNode *createLinkedListNode(void *data, LinkedListNode *next,
-				     LinkedlistNode *prev);
-bool destroyLinkedListNode(LinkedListNode *node);
+	struct LinkedListNode *next;
+	struct LinkedListNode *prev;
+};
+struct LinkedListNode *createLinkedListNode(void *data,
+					    struct LinkedListNode *next,
+					    struct LinkedListNode *prev);
+bool destroyLinkedListNode(struct LinkedListNode *node);
 
 typedef struct LinkedList {
 	size_t len;
-	LinkedListNode *start;
-	LinkedListNode *current;
+	struct LinkedListNode *start;
 } LinkedList;
 LinkedList *createLinkedList(size_t starting_size,
-			     LinkedListNode *starting_node);
+			     struct LinkedListNode *starting_node);
 bool destroyLinkedList(LinkedList *list);
-bool linkedList_add(LinkedList *list, LinkedListNode *node, size_t position);
-bool linkedList_remove(LinkedList *list, size_t position);
-size_t linkedlist_getPos(LinkedList *list, LinkedListNode *node);
-size_t linkedlist_getNode(LinkedList *list, size_t position);
+bool linkedList_verify(LinkedList *list);
+void linkedList_add(LinkedList *list, struct LinkedListNode *node,
+		    size_t position);
+size_t linkedList_getPos(LinkedList *list, struct LinkedListNode *node);
+struct LinkedListNode *linkedList_getNode(LinkedList *list, size_t position);
 
 #endif
