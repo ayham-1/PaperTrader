efac4d15c4da3595b212b7f45f8d6162451a22dc
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-06-14 15:10:06 +0300

    ds: add Transactiontype & Transatction definition

---
+++ src/libtrader/ds/account/transaction.rs
@@ -0,0 +1,13 @@
+#[derive(PartialEq, Debug)]
+pub enum TransactionType { Sell, Buy }
+impl Default for TransactionType {
+    fn default() -> Self { TransactionType::Buy }
+}
+
+#[derive(Default, PartialEq, Debug)]
+pub struct Transaction {
+    pub action_type: TransactionType,
+    pub cost: i64,
+    pub stocks_amount: i64,
+    pub is_open: bool
+}
