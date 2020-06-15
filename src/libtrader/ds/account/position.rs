d7f5bd381e6343d4348159d6b9453a6ce98267ec
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-06-15 06:44:35 +0300

    ds: remove trasnaction & add position

---
+++ src/libtrader/ds/account/position.rs
@@ -0,0 +1,22 @@
+use chrono::{DateTime, Utc}
+
+#[derive(PartialEq, Debug)]
+pub enum PositionType { Sell, Buy }
+impl Default for TransactionType {
+    fn default() -> Self { PositionType::Buy }
+}
+
+#[derive(Default, PartialEq, Debug)]
+pub struct Position {
+    pub action_type: PositionType,
+    pub stock_symbol: String,
+    pub stock_open_amount: i64,
+    pub stock_open_price: i64,
+    pub stock_open_cost: i64,
+    pub stock_close_amount: i64,
+    pub stock_close_price: i64,
+    pub stock_close_cost: i64,
+    pub open_date: DateTime<Utc>,
+    pub close_date: DateTime<Utc>,
+    pub is_open: bool,
+}
