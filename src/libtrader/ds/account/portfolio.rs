f5750add0640250d9416867043df5a3a45d4d8b2
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-06-15 07:17:06 +0300

    ds: add portfolio

--- src/libtrader/ds/account/portfolio.rs
+++ src/libtrader/ds/account/portfolio.rs
@@ -1,3 +1,7 @@
+use crate::ds::account::position::Position;
+
 #[derive(PartialEq, Debug)]
 pub struct Portfolio {
+    pub position_history: Vec<Position>,
+    pub open_positions: Vec<Position>
 }
