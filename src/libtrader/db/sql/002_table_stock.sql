76214069655f985d9f2aed49af9d8a39b68d15eb
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-01 13:56:21 +0300

    db: add 001_schema_asset & 002_table_stock

---
+++ src/libtrader/db/sql/002_table_stock.sql
@@ -0,0 +1,8 @@
+CREATE TABLE asset.stock_vals (
+	id					BIGSERIAL PRIMARY KEY
+	isin				text NOT NULL
+	time_since_epoch 	timestamp NOT NULL
+	ask_price			numeric NOT NULL
+	bid_price			numeric NOT NULL
+	volume				bigint NOT NULL
+)
