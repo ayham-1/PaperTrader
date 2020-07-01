9f4d5d0a796bdc2a59eba8282ce23ba93f2d8c08
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-01 13:14:58 +0300

    fix compilation error with removing unneeded ds

--- src/libtrader/ds/server/master_state.rs
+++ src/libtrader/ds/server/master_state.rs
@@ -1,11 +1,8 @@
-use std::collections::HashMap;
 use crate::ds::server::worker_server::WorkerServer;
 use crate::ds::account::session::SessionID;
-use crate::ds::asset::stock::Stock;
 
 #[derive(Default, PartialEq, Debug)]
 pub struct MasterState {
     pub worker_servers: Vec<WorkerServer>,
     pub active_sessions: Vec<SessionID>,
-    pub assets_data: HashMap<String, Stock>,
 }
