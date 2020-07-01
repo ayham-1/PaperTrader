25201f43398b36efc34eef3d132da6cb52fb8348
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-01 08:13:46 +0300

    db: add sql companies table

---
+++ src/libtrader/db/sql/000_table_companies.rs
@@ -0,0 +1,11 @@
+CREATE TABLE companies (
+    id               SERIAL PRIMARY KEY
+    symbol           text UNIQUE NOT NULL
+    isin             text UNIQUE NOT NULL
+    company_name     text NOT NULL
+    primary_exchange text NOT NULL
+    sector           text NOT NULL
+    industry         text NOT NULL
+    primary_sic_code text NOT NULL
+    employees        integer NOT NULL
+)
