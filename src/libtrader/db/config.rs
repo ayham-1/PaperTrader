57f802ab78870e9d776f0653a6dd9829052c4f5d
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-14 19:49:09 +0300

    add: basic unbuildable init()

---
+++ src/libtrader/db/config.rs
@@ -0,0 +1,9 @@
+static DB_HOST: &'static str = "localhost";
+static DB_HOST_PORT: &'static str = "5432";
+static DB_NAME: &'static str = "pt_db";
+
+static DB_USER: &'static str = "pt_usr";
+static DB_PASS: &'static str = "PASSWORD";
+
+static DB_SESS_USER: &'static str = "sessions_schema_usr";
+static DB_SESS_PASS: &'static str = "PASSWORD";
