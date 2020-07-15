496d93cc7d64d3f1f7250c50841645ab10777d64
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-15 05:53:31 +0300

    fix: now build is compilable

--- src/libtrader/db/config.rs
+++ src/libtrader/db/config.rs
@@ -1,9 +1,9 @@
-static DB_HOST: &'static str = "localhost";
-static DB_HOST_PORT: &'static str = "5432";
-static DB_NAME: &'static str = "pt_db";
+pub static DB_HOST: &'static str = "localhost";
+pub static DB_HOST_PORT: &'static str = "5432";
+pub static DB_NAME: &'static str = "pt_db";
 
-static DB_USER: &'static str = "pt_usr";
-static DB_PASS: &'static str = "PASSWORD";
+pub static DB_USER: &'static str = "pt_usr";
+pub static DB_PASS: &'static str = "PASSWORD";
 
-static DB_SESS_USER: &'static str = "sessions_schema_usr";
-static DB_SESS_PASS: &'static str = "PASSWORD";
+pub static DB_SESS_USER: &'static str = "sessions_schema_usr";
+pub static DB_SESS_PASS: &'static str = "PASSWORD";
--- src/libtrader/db/init.rs
+++ src/libtrader/db/init.rs
@@ -2,8 +2,8 @@ use crate::db::config::{*};
 
 use postgres::{Client, NoTls};
 
-pub fn init() -> Result< Client, Error> {
-    let mut client = Client::connect(format!("host={} port={} user={} db={} password={}", DB_HOST, DB_HOST_PORT, DB_USER, DB_PASS), NoTls);
+pub fn init() -> Result<Client, postgres::error::Error> {
+    let client = Client::connect(format!("host={} port={} user={} db={} password={}", DB_HOST, DB_HOST_PORT, DB_USER, DB_NAME, DB_PASS).as_str(), NoTls);
 
     return client;
 }
