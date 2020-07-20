462ee42f985e2aa54fbb6974128a2b725394924e
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-20 11:43:25 +0300

    add: sql init scripts execution

--- src/libtrader/db/sql/002_table_stock.sql
+++ src/libtrader/db/sql/002_table_stock.sql
@@ -1,8 +1,8 @@
 CREATE TABLE asset_schema.stock_vals (
-	id					BIGSERIAL PRIMARY KEY
-	isin				text NOT NULL
-	time_since_epoch 	timestamp NOT NULL
-	ask_price			numeric NOT NULL
-	bid_price			numeric NOT NULL
+	id					BIGSERIAL PRIMARY KEY,
+	isin				text NOT NULL,
+	time_since_epoch 	timestamp NOT NULL,
+	ask_price			numeric NOT NULL,
+	bid_price			numeric NOT NULL,
 	volume				bigint NOT NULL
 )
