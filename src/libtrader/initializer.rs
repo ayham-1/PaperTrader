e8055056c45d76added630e93bd6183fa78d73df
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-23 18:04:14 +0300

    fix: compilation

---
+++ src/libtrader/initializer.rs
@@ -0,0 +1,15 @@
+use crate::db::init::db_init;
+use crate::ds::server::global_state::GlobalState;
+
+pub fn libtrader_init() -> Result<GlobalState, String> {
+    let state: GlobalState = GlobalState::default();
+
+    match db_init() {
+        Ok(()) => {
+            println!("ran db_init");
+        },
+        Err(err) => return Err(format!("INIT_DB_FAILED: {}", err))
+    }
+
+    Ok(state)
+}
