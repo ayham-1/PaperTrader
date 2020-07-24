cb8a6ce2aaff836717315fbe2970ff49b18ebdb4
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-24 11:07:00 +0300

    add: company ds

---
+++ src/libtrader/ds/generic/company.rs
@@ -0,0 +1,11 @@
+pub struct Company {
+    pub id: u8,
+    pub symbol: String,
+    pub isin: String,
+    pub company_name: String,
+    pub primary_exchange: String,
+    pub sector: String,
+    pub industry: String,
+    pub primary_sic_code: String,
+    pub employees: u8,
+}
