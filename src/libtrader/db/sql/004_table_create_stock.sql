d42410ded3d081bf1456797f70836f0c5d611a5c
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-25 14:10:48 +0300

    add: basic structure for db cmds
    - add basic structure for db cmds
    - remove unneeded sql scripts. 004_table_create_stock and it's
    duplicate.

--- src/libtrader/db/sql/004_table_create_stock.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE asset_schema.stock_{} (
-	id					BIGSERIAL PRIMARY KEY,
-	isin				text NOT NULL,
-	time_since_epoch 	timestamp NOT NULL,
-	ask_price			bigint NOT NULL,
-	bid_price			bigint NOT NULL,
-	volume				bigint NOT NULL
-)
