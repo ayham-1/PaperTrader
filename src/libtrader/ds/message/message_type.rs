2b4fce04fed8473d7b0345654c884ba579ea9ebc
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:21:21 +0300

    add: display for MessageType

--- src/libtrader/ds/message/message_type.rs
+++ src/libtrader/ds/message/message_type.rs
@@ -4,3 +4,8 @@ pub enum MessageType {
     DataTransfer,
     ServerReturn
 }
+impl std::fmt::Display for MessageType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:#?}", self)
+    }
+}
