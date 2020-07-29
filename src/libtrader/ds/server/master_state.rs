222cfb3ed4039bdd85937dd570583905f9109ceb
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:21:43 +0300

    add: display for MasterState

--- src/libtrader/ds/server/master_state.rs
+++ src/libtrader/ds/server/master_state.rs
@@ -6,3 +6,8 @@ pub struct MasterState {
     pub worker_servers: Vec<WorkerServer>,
     pub active_sessions: Vec<SessionID>,
 }
+impl std::fmt::Display for MasterState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({:#?}, {:#?})", self.worker_servers, self.active_sessions)
+    }
+}
