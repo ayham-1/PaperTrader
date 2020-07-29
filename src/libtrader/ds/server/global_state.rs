55069bdb4d1dc320c9a204130b1829bfc4184c16
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:21:30 +0300

    add: display for GlobalState

--- src/libtrader/ds/server/global_state.rs
+++ src/libtrader/ds/server/global_state.rs
@@ -20,3 +20,8 @@ pub struct GlobalState {
     pub companies: HashMap<String, Company>, // symbol, company
     pub stock_vals: HashMap<String, StockVal>, // symbol, stockval
 }
+impl std::fmt::Display for GlobalState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({:#?}, {}, {:#?}, {:#?})", self.state, self.db_connect_str, self.companies, self.stock_vals)
+    }
+}
