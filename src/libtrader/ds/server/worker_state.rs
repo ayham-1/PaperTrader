ca74aec812026437059abd928924c9e42103cb3c
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:27:41 +0300

    add: display for WorkerState

--- src/libtrader/ds/server/worker_state.rs
+++ src/libtrader/ds/server/worker_state.rs
@@ -7,3 +7,8 @@ pub struct WorkerState {
     pub master_server_ip: Ipv4Addr,
     pub sessions: Vec<SessionID>,
 }
+impl std::fmt::Display for WorkerState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {:#?})", self.master_server_ip, self.sessions)
+    }
+}
