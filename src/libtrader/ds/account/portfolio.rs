14321bf70aa503a51ec18a7f787b33f95b0b9638
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:19:41 +0300

    add: display for Portfolio

--- src/libtrader/ds/account/portfolio.rs
+++ src/libtrader/ds/account/portfolio.rs
@@ -1,9 +1,13 @@
-use postgres_types::{ToSql, FromSql};
-
 use crate::ds::account::position::Position;
 
-#[derive(PartialEq, Debug, ToSql, FromSql)]
+#[derive(PartialEq, Debug)]
 pub struct Portfolio {
     pub position_history: Vec<Position>,
     pub open_positions: Vec<Position>
+}
+
+impl std::fmt::Display for Portfolio {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({:#?}, {:#?})", self.position_history, self.open_positions)
+    }
 }
