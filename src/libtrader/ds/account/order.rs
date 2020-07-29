8e7dafd4156dd6b9311fec519d5eedb129abf0c6
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:19:27 +0300

    add: display for Order

--- src/libtrader/ds/account/order.rs
+++ src/libtrader/ds/account/order.rs
@@ -9,7 +9,6 @@ pub struct Order {
     pub stock_filled: isize,
     pub is_filled: bool,
 }
-
 impl std::fmt::Display for Order {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {}, {}, {}, {}, {}, {})", self.action_type, self.stock_symbol, self.stock_price, self.stock_amount, self.stock_amount, self.stock_filled, self.is_filled)
