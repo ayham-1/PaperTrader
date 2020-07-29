d59e19aee5e5baea7cfde6aec15cea3283b3efe9
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:21:10 +0300

    add: display for Message

--- src/libtrader/ds/message/message.rs
+++ src/libtrader/ds/message/message.rs
@@ -10,3 +10,8 @@ pub struct Message {
     pub data_message_max: usize,
     pub data: Box<[char]>,
 }
+impl std::fmt::Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {}, {}, {}, {}, {:#?})", self.message_type, self.instruction, self.data_size, self.argument_count, self.data_message_number, self.data_message_max, self.data)
+    }
+}
