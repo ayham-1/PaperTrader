b9c52cf718fe4f5483c0a003e2c6b2603e6e1908
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-29 12:19:55 +0300

    add: display for Position

--- src/libtrader/ds/account/position.rs
+++ src/libtrader/ds/account/position.rs
@@ -2,6 +2,11 @@ use postgres_types::{ToSql, FromSql};
 
 #[derive(PartialEq, Debug, ToSql, FromSql)]
 pub enum PositionType { Sell, Buy }
+impl std::fmt::Display for PositionType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {PositionType::Sell=>"SELL", PositionType::Buy=>"BUY"})
+    }
+}
 
 #[derive(PartialEq, Debug, ToSql, FromSql)]
 pub struct Position {
@@ -16,4 +21,9 @@ pub struct Position {
     pub open_epoch: i64,
     pub close_epoch: i64,
     pub is_open: bool,
+}
+impl std::fmt::Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})", self.action_type, self.stock_symbol, self.stock_open_amount, self.stock_open_price, self.stock_open_cost, self.stock_close_amount, self.stock_close_price, self.stock_close_cost, self.open_epoch, self.close_epoch, self.is_open)
+    }
 }
