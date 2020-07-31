83c29c211debfbb5221ea27e7b48e76e94269b6a
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-07-31 10:42:57 +0300

    fix: wrong gitignore condition

---
+++ src/libtrader/misc/gen_log.rs
@@ -0,0 +1,42 @@
+pub fn gen_log() {
+    /* 
+     * Linux will use /var/log/papertrader/.
+     * macOS will use /var/log/papertrader/.
+     * Windows & other oses will output to a file in the current directory.
+     * */
+    use simplelog::*;
+    use std::fs::File;
+
+    match os_type::current_platform().os_type {
+        os_type::OSType::Unknown => {
+            match std::fs::create_dir("log") {
+                Ok(()) => {},
+                Err(err) => panic!("GEN_LOG_FAILED_DIR_CREATION: {}", err)
+            };
+            CombinedLogger::init(vec![
+                                 #[cfg(debug_assertions)]
+                                 TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed),
+                                 #[cfg(not(debug_assertions))]
+                                 TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+                                 WriteLogger::new(LevelFilter::Info, Config::default(), 
+                                                  File::create(format!("log/log-{}.txt", 
+                                                                       chrono::Utc::now().to_rfc2822())).unwrap())
+            ]).unwrap();
+        },
+        _ => {
+            match std::fs::create_dir("/var/log/papertrader/") {
+                Ok(()) => {},
+                Err(err) => panic!("GEN_LOG_FAILED_DIR_CREATION: {}", err)
+            };
+            CombinedLogger::init(vec![
+                                 #[cfg(debug_assertions)]
+                                 TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed),
+                                 #[cfg(not(debug_assertions))]
+                                 TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+                                 WriteLogger::new(LevelFilter::Info, Config::default(), 
+                                                  File::create(format!("/var/log/papertrader/log-{}.txt", 
+                                                                       chrono::Utc::now().to_rfc2822())).unwrap())
+            ]).unwrap();
+        }
+    };
+}
