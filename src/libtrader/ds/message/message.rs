0f1046533b448496d531dbab5dbc7895c7f91061
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-04 15:38:47 +0300

    parser: make data as u8 array, and remove data_size

--- src/libtrader/ds/message/message.rs
+++ src/libtrader/ds/message/message.rs
@@ -4,15 +4,14 @@ use crate::ds::message::message_type::{MessageType};
 pub struct Message {
     pub message_type: MessageType,
     pub instruction: i64,
-    pub data_size: usize,
     pub argument_count: usize,
     pub data_message_number: usize,
     pub data_message_max: usize,
-    pub data: Box<[char]>,
+    pub data: [u8],
 }
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {}, {}, {}, {}, {}, {:#?})", self.message_type, self.instruction, self.data_size, 
+        write!(f, "({}, {}, {}, {}, {}, {:#?})", self.message_type, self.instruction, 
                self.argument_count, self.data_message_number, self.data_message_max, self.data)
     }
 }
