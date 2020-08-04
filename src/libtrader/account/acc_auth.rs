969d0eb83f68b5f1aedfa73a85536bd313e0f410
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-04 15:00:40 +0300

    account: add acc_auth_client()

---
+++ src/libtrader/account/acc_auth.rs
@@ -0,0 +1,58 @@
+use ring::rand::SecureRandom;
+use ring::{digest, pbkdf2, rand};
+use std::num::NonZeroU32;
+
+pub fn acc_auth_client(username: &str, email: &str, password: &str) -> Result<(), String> {
+    let rng = rand::SystemRandom::new();
+
+    /* 
+     * hash the username 
+     * */
+    /* generate false client salt TODO: get salt from the server */
+    let mut user_client_salt = [0u8; digest::SHA512_OUTPUT_LEN/2];
+    rng.fill(&mut user_client_salt).unwrap();
+
+    let mut hashed_usr = [0u8; digest::SHA512_OUTPUT_LEN];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA512,
+        NonZeroU32::new(100_000).unwrap(),
+        &user_client_salt,
+        username.as_bytes(),
+        &mut hashed_usr);
+
+    /*
+     * hash the email
+     */
+    /* generate false client salt TODO: get salt from the server */
+    let mut email_client_salt = [0u8; digest::SHA512_OUTPUT_LEN/2];
+    rng.fill(&mut email_client_salt).unwrap();
+
+    let mut hashed_email = [0u8; digest::SHA512_OUTPUT_LEN];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA512,
+        NonZeroU32::new(175_000).unwrap(),
+        &email_client_salt,
+        email.as_bytes(),
+        &mut hashed_email);
+
+    /*
+     * hash the password
+     */
+    /* generate false client salt TODO: get salt from the server */
+    let mut password_client_salt = [0u8; digest::SHA512_OUTPUT_LEN/2];
+    rng.fill(&mut password_client_salt).unwrap();
+
+    let mut hashed_password = [0u8; digest::SHA512_OUTPUT_LEN];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA512,
+        NonZeroU32::new(250_000).unwrap(),
+        &password_client_salt,
+        email.as_bytes(),
+        &mut hashed_password);
+
+    /* TODO: send the data to the server and return a session id */
+
+    Ok(()) 
+}
+
+pub fn acc_auth_server() -> Result<(), String> { Ok(()) }
--- src/libtrader/ds/account/account.rs
+++ src/libtrader/ds/account/account.rs
@@ -3,11 +3,18 @@ use crate::ds::account::portfolio::Portfolio;
 #[derive(PartialEq, Debug)]
 pub struct Account {
     pub username: String,
+    pub server_username_salt: String,
+    pub client_username_salt: String,
+
     pub email: String,
-    pub is_pass: bool,
+    pub server_email_salt: String,
+    pub client_email_salt: String,
+
     pub pass_hash: String,
-    pub pass_salt: String,
+    pub server_pass_salt: String,
     pub client_pass_salt: String,
+
+    pub is_pass: bool,
     pub portfolio: Portfolio,
     pub transactions: Vec<f64>
 }
