2ce53955b9576f0743b921ba93241c1e2a6fe492
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-06 12:56:35 +0300

    doc: add lookup_ipv4()

--- src/libtrader/misc/lookup_ipv4.rs
+++ src/libtrader/misc/lookup_ipv4.rs
@@ -1,5 +1,12 @@
 use std::net::{SocketAddr, ToSocketAddrs};
 
+/// Looks up a string hostname and port, returns a SocketAddr represnting that hostname and port.
+///
+/// Arguments:
+/// host - hostname to search.
+/// port - port on hostname to search.
+///
+/// Returns: SocketAddr
 pub fn lookup_ipv4(host: &str, port: u16) -> SocketAddr {
 
     let addrs = (host, port).to_socket_addrs().unwrap();
