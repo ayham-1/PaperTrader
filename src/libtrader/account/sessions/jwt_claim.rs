d2d6dea5534eac92527734d7e4a0b6c657bb4efc
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-09 12:11:40 +0300

    doc: add JWTClaim struct

--- src/libtrader/account/sessions/jwt_claim.rs
+++ src/libtrader/account/sessions/jwt_claim.rs
@@ -3,6 +3,11 @@ use serde::{Deserialize, Serialize};
 #[cfg(not(feature="client"))]
 pub static JWT_SECRET: &'static str = "seecreet";
 
+/// JWT Claim representing an authorized user.
+///
+/// Members:
+/// user_id - The user id that is authorized.
+/// exp - The unix epoch at which this claim expires.
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct JWTClaim {
     pub user_id: usize,
