8a1d9e83f6b7144b0e80d601ce39bcde67032d38
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-10 07:30:45 +0300

    doc: add hash()

--- src/libtrader/account/hash.rs
+++ src/libtrader/account/hash.rs
@@ -1,6 +1,21 @@
 use ring::{digest, pbkdf2};
 use std::num::NonZeroU32;
 
+/// A generic hashing abstraction function.
+/// 
+/// Useful for quickly swapping the current hashing system.
+///
+/// Arguments:
+/// val - The value to be hashed.
+/// salt - The whole salt to be used.
+/// iter - The number of iteration to use.
+///
+/// Returns: u8 array of size 64 bytes.
+///
+/// Example:
+/// ```rust
+///     let email_hash = hash("test@test.com", [0u8; 64], 124000);
+/// ```
 pub fn hash(val: &str, salt: [u8; digest::SHA512_OUTPUT_LEN], iter: u32) -> [u8; digest::SHA512_OUTPUT_LEN] {
     let iterations: NonZeroU32 = NonZeroU32::new(iter).unwrap();
 
