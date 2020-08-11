e6b8c265f50f2968f77ecb55e05422776d5ac47d
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-11 17:57:27 +0300

    make hybrid buildable

--- src/libtrader/libtrader.rs
+++ src/libtrader/libtrader.rs
@@ -1,8 +1,5 @@
-#[cfg(feature="client")] #[macro_use] extern crate log;
-#[cfg(feature="client")] #[macro_use] extern crate arrayref;
-
-#[cfg(feature="server")] #[macro_use] extern crate log;
-#[cfg(feature="server")] extern crate arrayref;
+#[cfg(any(feature="server", feature="client"))] #[macro_use] extern crate log;
+#[cfg(any(feature="client", feature="client"))] #[macro_use] extern crate arrayref;
 
 #[cfg(featue="server")] #[macro_use] extern crate lazy_static;
 extern crate simplelog;
