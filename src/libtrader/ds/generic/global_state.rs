ef8b6e2d657d5359877a16d8e1979a53d3a4c7d0
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-11 12:09:11 +0300

    add: global global state

--- src/libtrader/ds/generic/global_state.rs
+++ src/libtrader/ds/generic/global_state.rs
@@ -3,6 +3,9 @@ use std::collections::HashMap;
 use crate::ds::generic::company::Company;
 use crate::ds::generic::stock_val::StockVal;
 
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
 #[derive(PartialEq, Debug)]
 pub struct GlobalState {
     pub db_connect_str: String,
@@ -23,4 +26,8 @@ impl std::fmt::Display for GlobalState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {:#?}, {:#?})", self.db_connect_str, self.companies, self.stock_vals)
     }
+}
+
+lazy_static! {
+    pub static ref GLOBAL_STATE: Mutex<GlobalState> = Mutex::new(GlobalState::default());
 }
