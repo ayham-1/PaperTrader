b2c6952b4d83d1a319eacca4655f5d683ac22b41
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-12 09:14:36 +0300

    add: login_normal()

--- src/libtrader/server/db/cmd/mod.rs
+++ src/libtrader/server/db/cmd/mod.rs
@@ -1,4 +1,9 @@
 pub mod create_company;
 pub mod get_company;
+
 pub mod create_stock;
 pub mod get_stock;
+
+pub mod get_user_salt;
+pub mod get_user_hash;
+pub mod user_exists;
---
+++ src/libtrader/server/network/cmd/login_normal.rs
@@ -0,0 +1,25 @@
+use std::io::Write;
+
+use crate::common::message::message::Message;
+use crate::common::message::message_type::MessageType;
+use crate::common::message::message_builder::message_builder;
+
+use crate::server::network::tls_connection::TlsConnection;
+use crate::server::account::authorization::acc_auth;
+
+pub fn login_normal(tls_connection: &mut TlsConnection, message: &Message) {
+    /* assert recieved message */
+    if message.msgtype != MessageType::Command || message.argument_count != 3
+        || message.data_message_number != 0 || message.data_message_max != 0
+           || message.data.len()  == 0 {
+               warn!("LOGIN_INVALID_MESSAGE");
+               tls_connection.closing = true;
+               return;
+           }
+
+    /* call acc_auth() server version */
+    match acc_auth(tls_connection, message) {
+        Ok(_) => {},
+        Err(_) => {}
+    };
+}
