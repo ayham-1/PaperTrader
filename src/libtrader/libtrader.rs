0ebc30bdb43410b069827ec8db0c1288e06ce4dc
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-13 16:55:30 +0300

    fix: hybrid build

--- src/libtrader/libtrader.rs
+++ src/libtrader/libtrader.rs
@@ -1,8 +1,6 @@
 #[cfg(any(feature="server", feature="client"))] #[macro_use] extern crate log;
-#[cfg(feature="server")] extern crate arrayref;
-#[cfg(feature="server")] extern crate json;
-#[cfg(feature="client")] #[macro_use] extern crate arrayref;
-#[cfg(feature="client")] #[macro_use] extern crate json;
+#[cfg(any(feature="server", feature="client"))] #[macro_use] extern crate arrayref;
+#[cfg(any(feature="server", feature="client"))] #[macro_use] extern crate json;
 
 extern crate simplelog;
 extern crate os_type;
