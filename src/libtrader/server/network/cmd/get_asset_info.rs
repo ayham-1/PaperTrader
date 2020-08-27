61cd3bc1d11579ce1ccc827b62fb6885a45eae6b
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-27 14:00:02 +0300

    abstract: get_asset_info

--- src/libtrader/server/network/cmd/get_asset_info.rs
+++ src/libtrader/server/network/cmd/get_asset_info.rs
@@ -10,13 +10,11 @@ use crate::server::db::cmd::get_company::get_company_from_db;
 
 pub fn get_asset_info(tls_connection: &mut TlsConnection, message: &Message) {
     /* assert recieved message */
-    if message.msgtype != MessageType::DataTransfer || message.argument_count != 1 
-        || message.data_message_number != 0 || message.data_message_max != 0
-            || message.data.len() == 0 {
-                warn!("GET_ASSET_INFO_INVALID_MESSAGE");
-                tls_connection.closing = true;
-                return;
-            }
+    if assert_msg(message, MessageType::DataTransfer, 1, 0, 0, 0) {
+        tls_connection.closing = true;
+        warn!("GET_ASSET_INFO_INVALID_MESSAGE");
+        return;
+    }
 
     /* call get_company_from_db() */
     match get_company_from_db(bincode::deserialize(&message.data).unwrap()) {
