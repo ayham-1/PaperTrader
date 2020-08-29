aa090d422e472ba83d9942549f6870e48b1dd4e2
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-08-29 10:53:49 +0300

    abstract: retrieve_portfolio

--- src/libtrader/server/network/cmd/login_normal.rs
+++ src/libtrader/server/network/cmd/login_normal.rs
@@ -11,8 +11,8 @@ use crate::server::account::authorization::acc_auth;
 pub fn login_normal(tls_connection: &mut TlsConnection, message: &Message) {
     /* assert recieved message */
     if assert_msg(message, MessageType::Command, 3, 0, 0, 0) {
-        warn!("LOGIN_INVALID_MESSAGE");
         tls_connection.closing = true;
+        warn!("LOGIN_INVALID_MESSAGE");
         return;
     }
 
--- src/libtrader/server/network/cmd/retrieve_portfolio.rs
+++ src/libtrader/server/network/cmd/retrieve_portfolio.rs
@@ -1,17 +1,16 @@
 use crate::common::message::message::Message;
 use crate::common::message::message_type::MessageType;
+use crate::common::misc::assert_msg::assert_msg;
 
 use crate::server::network::tls_connection::TlsConnection;
 use crate::server::account::retrieval_portfolio::acc_retrieve_portfolio;
 
 pub fn retrieve_portfolio(tls_connection: &mut TlsConnection, message: &Message) {
     /* assert recieved message */
-    if message.msgtype != MessageType::Command || message.argument_count != 1
-        || message.data_message_number != 0 || message.data_message_max != 0
-            || message.data.len() == 0 {
-                warn!("RETRIEVE_PORTFOLIO_INVALID_MESSAGE");
-                tls_connection.closing = true;
-                return;
+    if assert_msg(message, MessageType::Command, 1, 0, 0, 0) {
+        tls_connection.closing = true;
+        warn!("RETRIEVE_PORTFOLIO_INVALID_MESSAGE");
+        return;
     }
 
     /* call acc_retrieve_portfolio() server version */
