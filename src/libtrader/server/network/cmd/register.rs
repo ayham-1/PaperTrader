69716a09f7ae140b0a85d43d4fba546ecac95d19
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-09-10 06:19:42 +0300

    update

--- src/libtrader/server/network/cmd/register.rs
+++ src/libtrader/server/network/cmd/register.rs
@@ -22,6 +22,6 @@ pub fn register(tls_connection: &mut TlsConnection, message: &Message) {
             let message = message_builder(MessageType::ServerReturn, 1, 0, 0, 0, Vec::new());
             let _ = tls_connection.write(&bincode::serialize(&message).unwrap());
         },
-        Err(err) => warn!("REGISTER_FAILED: " /*{}", err*/) // TODO: OUTPUT ERRORS
+        Err(_) => warn!("REGISTER_FAILED: " /*{}", err*/) // TODO: OUTPUT ERRORS
     };
 }
