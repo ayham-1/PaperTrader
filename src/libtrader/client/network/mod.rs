ca7c91216b41a3731674ed8ea98411b254867492
ayham <altffour@protonmail.com>
2021-06-01 19:04:28 +0300

    cargo fmt

--- src/libtrader/client/network/mod.rs
+++ src/libtrader/client/network/mod.rs
@@ -1,3 +1,3 @@
 pub mod cmd;
-pub mod handle_data;
 pub mod gen_tls_client_config;
+pub mod handle_data;
--- src/libtrader/server/network/mod.rs
+++ src/libtrader/server/network/mod.rs
@@ -1,4 +1,4 @@
 pub mod cmd;
+pub mod gen_tls_server_config;
 pub mod handle_data;
 pub mod jwt_wrapper;
-pub mod gen_tls_server_config;
